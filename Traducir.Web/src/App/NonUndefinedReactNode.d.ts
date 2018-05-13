55f335f5e5449e3ad262e66d08c5f7a266faf23c
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2018-05-13 15:05:41 -0500

    Exclude<T, U> was already defined in lib.d.ts

--- Traducir.Web/src/App/NonUndefinedReactNode.d.ts
+++ Traducir.Web/src/App/NonUndefinedReactNode.d.ts
@@ -1,7 +1,5 @@
 import { ReactNode } from "react";
 
-type Exclude<T, ShouldNotBeT> = T extends ShouldNotBeT ? never : T;
-
 // React.Component.render() should return NonUndefinedReactNode insetad of React.ReactNode since undefined is not allowed
 // helper render methods can actually return React.ReactNode
 type NonUndefinedReactNode = Exclude<ReactNode, undefined>
