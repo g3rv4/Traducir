6ffeb6d37b41ea27f1743500f2e6e8fcc37cd8eb
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2018-05-14 07:47:31 -0500

    Using conditional types to create a more readable NonUndefinedReactNode type definition (#91)

--- Traducir.Web/src/App/NonUndefinedReactNode.d.ts
+++ Traducir.Web/src/App/NonUndefinedReactNode.d.ts
@@ -1,5 +1,5 @@
-import { ReactChild, ReactFragment, ReactPortal } from "react";
+import { ReactNode } from "react";
 
 // React.Component.render() should return NonUndefinedReactNode insetad of React.ReactNode since undefined is not allowed
 // helper render methods can actually return React.ReactNode
-type NonUndefinedReactNode = ReactChild | ReactFragment | ReactPortal | string | number | boolean | null;
+type NonUndefinedReactNode = Exclude<ReactNode, undefined>
