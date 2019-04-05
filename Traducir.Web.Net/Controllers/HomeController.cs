95523b8ee093addf3018a03b689f88766c8f8214
Nestor Soriano Vilchez <konamiman@konamiman.com>
2019-04-05 13:34:03 +0200

    Create the ASP.Net project and configure it appropriately:
    - Create the project named Asp.Net.Web, targetting ASP.Net Core 2.2
    
    - Add Core and Migrations project references
    
    - Add controllers and models from Traducir.Api as linked files
    
    - Add the same NuGet packages that Traducir.Api has
    
    - Run setup configuration from Traducir.Api (via linked Setup.cs file)

---
+++ Traducir.Web.Net/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Traducir.Web.Net.Models;
+
+namespace Traducir.Web.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
