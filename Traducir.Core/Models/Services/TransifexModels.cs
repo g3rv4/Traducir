fa99c20b8e6e52d5ed04783aac6686d8023dc063
Dan Roberts <dangger36@gmail.com>
2022-04-14 15:11:28 +0100

    Remove Transifex V2 integration code (#141)

--- Traducir.Core/Models/Services/TransifexModels.cs
+++ Traducir.Core/Models/Services/TransifexModels.cs
@@ -12,30 +12,20 @@ namespace Traducir.Core.Models.Services
 
         public string NormalizedKey => _normalizedKey ?? (_normalizedKey = Key.ToNormalizedKey());
 
-        [DataMember(Name = "reviewed")]
-        public bool Reviewed { get; set; }
-
         [DataMember(Name = "source_string")]
         public string Source { get; set; }
 
         [DataMember(Name = "translation")]
-        public string UnreviewedTranslation { get; set; }
+        public string Translation { get; set; }
 
         [DataMember(Name = "comment")]
         public string Comment { get; set; }
 
         public string Variant => Comment.HasValue() ? Comment : null;
-
-        public string Translation => Reviewed ? UnreviewedTranslation : null;
     }
 
     public class TransifexStringToPush
     {
-        #pragma warning disable CA1822
-        [DataMember(Name = "reviewed")]
-        public bool Reviewed => true;
-        #pragma warning restore CA1822
-
         [IgnoreDataMember]
         public string Key { get; set; }
 
